cd8e96b3251a3a7bea14000fab8fcd7ef738b41e
LUSEDOU <lusedou@gmail.com>
2023-09-23 21:49:46 -0500

    update: ok2

--- symlink.sh
+++ symlink.sh
@@ -4,5 +4,6 @@ ln -s ~/dotfiles/.p10k.zsh ~/.p10k.zsh &&
 ln -s ~/dotfiles/my_aliases.zsh  $ZSH_CUSTOM/my_aliases.zsh &&
 ln -s ~/dotfiles/my_paths.zsh  $ZSH_CUSTOM/my_paths.zsh &&
 ln -s ~/dotfiles/tmux/ ~/.tmux &&
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf &&
 ln -s ~/dotfiles/nvim ~/.config/nvim &&
 
