a011eba4ae23eb8fd1082769385bc0038a7a1cb5
LUSEDOU <lusedou@gmail.com>
2023-09-28 23:21:29 -0500

    preview

--- nvim/after/plugin/markdown-preview.lua
+++ nvim/after/plugin/markdown-preview.lua
@@ -1,3 +1,5 @@
 vim.keymap.set('n', '<leader>mpp', '<cmd>MarkdownPreview<cr>')
 vim.keymap.set('n', '<leader>mpt', '<cmd>MarkdownPreviewToggle<cr>')
 vim.keymap.set('n', '<leader>mps', '<cmd>MarkdownPreviewStop<cr>')
+
+vim.g.MarkdownPreview_browser = 'librewolf'
