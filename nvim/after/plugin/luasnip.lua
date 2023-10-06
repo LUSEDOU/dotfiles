cd6432e0af4dbba345434d53ddcf4002de34c70a
LUSEDOU <lusedou@gmail.com>
2023-10-06 15:28:07 -0500

    fix(nvim): disable autosnippet error

--- nvim/after/plugin/luasnip.lua
+++ nvim/after/plugin/luasnip.lua
@@ -61,7 +61,6 @@ ls.add_snippets('all', {
             dscr = "An autotriggering snippet that expands 'hi' into 'Hello, world!'",
             regTrig = false,
             priority = 100,
-            snippetType = "autosnippet"
         },
         {                       -- Table 2: snippet nodes (don't worry about this for now---we'll cover nodes shortly)
             t("Hello, world!"), -- A single text node
