958b7c300dd12bcfd62390871beb1fa6af8dc1dc
LUSEDOU <dolorier_uribe@hotmail.com>
2023-12-26 14:07:39 -0500

    feat(zsh): bin support

--- zsh/.oh-my-zsh/custom/my_paths.zsh
+++ zsh/.oh-my-zsh/custom/my_paths.zsh
@@ -1,5 +1,8 @@
 export PATH="/usr/local/anaconda3/bin:$PATH"
 path+=($FDK)
 path+=($HOME/.pub-cache/bin)
+path+=($HOME/.local/bin/scripts)
 # path+=('/Users/lusedou/fvm/default/bin')
 export PATH
+
+export VISUAL=ewrap
