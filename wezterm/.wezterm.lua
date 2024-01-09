ecbffba4e2aeb6985af6310d1ae83bcbedb10418
Luis Sebastian Dolorier Uribe <dolurilu@central.perufarma.com.pe>
2024-01-09 08:45:20 -0500

    fix

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,12 +7,12 @@ if wezterm.config_builder then
 end
 
 config.font = wezterm.font 'MesloLGM Nerd Font'
-config.color_scheme = 'Mona Lisa'
-config.window_background_opacity = 0.8
+config.color_scheme = 'MonaLisa'
+-- config.window_background_opacity = 0.8
 config.scrollback_lines = 6000
 
 --- Set Pwsh as the default on Windows
-config.default_prog = { 'pwsh.exe', '-NoLogo' }
+config.default_prog = { 'pwsh.exe' }
 
 
 return config
