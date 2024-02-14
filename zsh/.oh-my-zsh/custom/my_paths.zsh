e210c7643dc60d7b0e6296c85f69935d3c8231b8
LUSEDOU <dolorier_uribe@hotmail.com>
2024-02-14 01:11:18 -0500

    ?

--- zsh/.oh-my-zsh/custom/my_paths.zsh
+++ zsh/.oh-my-zsh/custom/my_paths.zsh
@@ -1,8 +1,6 @@
 export PATH="/usr/local/anaconda3/bin:$PATH"
-path+=($FDK)
+# path+=($FDK)
 path+=($HOME/.pub-cache/bin)
 path+=($HOME/.local/bin/scripts)
 # path+=('/Users/lusedou/fvm/default/bin')
 export PATH
-
-export VISUAL=ewrap
