9a2ffd0b6a4969dd40230d9ec3fe7c0f11ecc992
LUSEDOU <dolorier_uribe@hotmail.com>
2024-03-02 01:40:03 -0500

    feat: harpoon

--- nvim/.config/nvim/lua/custom/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/custom/plugins/harpoon.lua
@@ -1,16 +1,36 @@
 return {
     'ThePrimeagen/harpoon',
     branch = 'harpoon2',
-    opts = {},
     config = function()
+        local nmap = require("lusedou.keymaps").nmap
         local harpoon = require("harpoon")
 
-        vim.keymap.set('n', "<leader>a", function() harpoon:list():append() end)
-        vim.keymap.set('n', '<C-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        harpoon:setup({})
 
-        vim.keymap.set('n', '<C-h>', function() harpoon:list():select(1) end)
-        vim.keymap.set('n', '<C-t>', function() harpoon:list():select(2) end)
-        vim.keymap.set('n', '<C-n>', function() harpoon:list():select(3) end)
-        vim.keymap.set('n', '<C-s>', function() harpoon:list():select(4) end)
+        nmap {
+            "<leader>a",
+            command = function()
+                harpoon:list():append()
+            end,
+            desc = "Add current file"
+        }
+
+        nmap {
+            "<C-e>",
+            command = function()
+                harpoon.ui:toggle_quick_menu(harpoon:list())
+            end,
+            desc = "Toggle quick menu"
+        }
+
+        for i, key in ipairs({ 'h', 't', 'n', 's' }) do
+            nmap {
+                "<C-" .. key .. ">",
+                command = function()
+                    harpoon:list():select(i)
+                end,
+                desc = "Select " .. key
+            }
+        end
     end
 }
