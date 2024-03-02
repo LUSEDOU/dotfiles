6e056d057faf6c40ff47b2e1fcaf760962936bf9
LUSEDOU <dolorier_uribe@hotmail.com>
2024-03-02 11:18:50 -0500

    trouble

--- nvim/.config/nvim/lua/custom/plugins/trouble.lua
+++ nvim/.config/nvim/lua/custom/plugins/trouble.lua
@@ -1,63 +1,17 @@
--- local trouble = require('trouble')
---
--- vim.keymap.set("n", "<leader>xx", function() trouble.toggle() end, { desc = 'Show troubles' })
--- vim.keymap.set("n", "<leader>xw", function() trouble.toggle("workspace_diagnostics") end, { desc = 'Show Workspace' })
--- vim.keymap.set("n", "<leader>xd", function() trouble.toggle("document_diagnostics") end, { desc = 'Show Document' })
--- vim.keymap.set("n", "<leader>xq", function() trouble.toggle("quickfix") end, { desc = 'Show Quickfix' })
--- vim.keymap.set("n", "<leader>xl", function() trouble.toggle("loclist") end, { desc = 'Show LocList' })
--- vim.keymap.set("n", "gR", function() trouble.toggle("lsp_references") end, { desc = 'Show Lsp References' })
 return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {},
-  config = function()
+  keys = function()
     local trouble = require('trouble')
-    local nmap = require('lusedou.keymaps').nmap
 
-    nmap {
-      '<leader>xx',
-      command = trouble.toggle,
-      desc = 'Show troubles',
-    }
-
-    nmap {
-      '<leader>xw',
-      command = function()
-        trouble.toggle("workspace_diagnostics")
-      end,
-      desc = 'Show Workspace',
-    }
-
-    nmap {
-      '<leader>xd',
-      command = function()
-        trouble.toggle("document_diagnostics")
-      end,
-      desc = 'Show Document',
-    }
-
-    nmap {
-      '<leader>xq',
-      command = function()
-        trouble.toggle("quickfix")
-      end,
-      desc = 'Show Quickfix',
-    }
-
-    nmap {
-      '<leader>xl',
-      command = function()
-        trouble.toggle("loclist")
-      end,
-      desc = 'Show LocList',
-    }
-
-    nmap {
-      'gR',
-      command = function()
-        trouble.toggle("lsp_references")
-      end,
-      desc = 'Show Lsp References',
+    return {
+      { '<leader>xx', function() trouble.toggle() end,                        { desc = 'Show troubles' } },
+      { '<leader>xw', function() trouble.toggle("workspace_diagnostics") end, { desc = 'Show Workspace' } },
+      { '<leader>xd', function() trouble.toggle("document_diagnostics") end,  { desc = 'Show Document' } },
+      { '<leader>xq', function() trouble.toggle("quickfix") end,              { desc = 'Show Quickfix' } },
+      { '<leader>xl', function() trouble.toggle("loclist") end,               { desc = 'Show LocList' } },
+      { 'gR',         function() trouble.toggle("lsp_references") end,        { desc = 'Show Lsp References' } },
     }
   end,
+  opts = {},
 }
