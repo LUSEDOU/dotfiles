c3ff47cc33a4520f89c4e5e11692d2eef47b4cf2
LUSEDOU <dolorier_uribe@hotmail.com>
2024-03-02 11:18:46 -0500

    treesitter

---
+++ nvim/.config/nvim/lua/custom/plugins/treesitter.lua
@@ -0,0 +1,32 @@
+return {
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        {
+            "nvim-treesitter/nvim-treesitter-context",
+            keys = {
+                {
+                    '[c',
+                    function()
+                        require('treesitter-context').go_to_context(vim.v.count1())
+                    end,
+                    desc = 'Previous context'
+                },
+                { ']c', function() require('treesitter-context').next() end, desc = 'Next context' },
+            },
+        },
+    },
+    build = ':TSUpdate',
+    opts = {
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go", "bash", 'dart', },
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+        },
+    },
+    config = function(_, opts)
+        require('nvim-treesitter.configs').setup(opts)
+    end
+}
