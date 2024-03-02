41c79971524c81f0ae55724f3f61838ef23e80f5
LUSEDOU <dolorier_uribe@hotmail.com>
2024-03-02 11:59:44 -0500

    luasnip

--- nvim/.config/nvim/lua/custom/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/custom/plugins/luasnip.lua
@@ -1,5 +1,7 @@
 return {
     'L3MON4D3/LuaSnip',
+    version = "v2.*",
+    build = "make install_jsregexp",
     config = function()
         local ls = require('luasnip')
         local extras = require('luasnip.extras')
