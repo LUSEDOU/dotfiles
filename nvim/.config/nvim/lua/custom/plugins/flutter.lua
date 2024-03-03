b8a33b570c1ead497f49666e05064af09ef14420
LUSEDOU <dolorier_uribe@hotmail.com>
2024-03-03 11:29:33 -0500

    ?

--- nvim/.config/nvim/lua/custom/plugins/flutter.lua
+++ nvim/.config/nvim/lua/custom/plugins/flutter.lua
@@ -10,7 +10,22 @@ return {
             analysis = {
                 vim.fn.expand("$HOME/AppData/Local/Pub/Cache"),
             }
-        }
+        },
+        -- lsp = {
+        --     on_attach = function(client, bufnr)
+        --         print('Flutter tools attached')
+        --         local nmap = require('lusedou.keymaps').nmap
+        --
+        --         opts = { buffer = bufnr, remap = false }
+        --
+        --         nmap {
+        --             '<leader>gd',
+        --             command = vim.lsp.buf.definition,
+        --             desc = 'view definition',
+        --             opts = opts,
+        --         }
+        --     end
+        -- }
     },
     config = true,
 }
