660a600633833f778197bedb932ddaa1c754fcaa
LUSEDOU <107129219+LUSEDOU@users.noreply.github.com>
2024-03-06 18:31:48 -0500

    Nvim (#3)
    * refactor
    
    * feat: trouble
    
    * feat: harpoon
    
    * feat: zenmode
    
    * feat: autopair
    
    * feat: copilot
    
    * ?
    
    * enhance: harpoon
    
    * fix: harpoon
    
    * fix: copilot
    
    * add autoapirs
    
    * fix copilot
    
    * add zenmode
    
    * lazy colorscheme
    
    * flutter
    
    * neotest
    
    * treesitter
    
    * trouble
    
    * luasnip
    
    * fzf
    
    * init
    
    * colors
    
    * lsp
    
    * markdown
    
    * telescope
    
    * undootree
    
    * vimtex
    
    * treesitter
    
    * luasnip
    
    * init
    
    * ?
    
    * feat: update tmux-sessionizer
    
    * feat(flutter): load when dart
    
    * enahcnes
    
    * fix lsp
    
    * luasnip lazy load
    
    * fix harpoon
    
    * fixing lsp
    
    * fix lsp
    
    * lazy load treesitter
    
    * lazyload
    
    * ?
    
    * enhance(pwsh): nvim when dotfile
    
    * feat(bin): add localtodo script
    
    * enhance(zsh): remove neofetch from zshrc
    
    * feat(tmux): add localtodo keybinding
    
    * feat(nvim): lsp
    
    ---------
    
    Co-authored-by: dolorier_uribe@hotmail.com <ldolorier@perufarma.com.pe>

--- nvim/.config/nvim/lua/custom/plugins/lsp.lua
+++ nvim/.config/nvim/lua/custom/plugins/lsp.lua
@@ -1,132 +1,3 @@
--- return {
---     {
---         'deathbeam/lspecho.nvim',
---         opts = {
---             echo = true,  -- Echo progress messages, if set to false you can use .message() to get the current message
---             decay = 3000, -- Message decay time in milliseconds
---         },
---         event = 'BufReadPre',
---         config = true,
---     },
---     {
---         'williamboman/mason.nvim',
---         lazy = false,
---         config = true,
---     },
---     {
---         'VonHeikemen/lsp-zero.nvim',
---         branch = 'v3.x',
---         lazy = true,
---         config = false,
---         init = function()
---             -- Disable automatic setup, we are doing it manually
---             vim.g.lsp_zero_extend_cmp = 0
---             vim.g.lsp_zero_extend_lspconfig = 0
---         end,
---     },
---     -- Autocompletion
---     {
---         'hrsh7th/nvim-cmp',
---         event = 'InsertEnter',
---         dependencies = {
---             -- { 'L3MON4D3/LuaSnip' },
---             { 'hrsh7th/cmp-nvim-lsp' }, -- Required
---             { 'hrsh7th/cmp-nvim-lua' },
---             { 'hrsh7th/cmp-buffer' },
---             { 'hrsh7th/cmp-path' },
---             { 'saadparwaiz1/cmp_luasnip' },
---         },
---         config = function()
---             -- Here is where you configure the autocompletion settings.
---             local lsp_zero = require('lsp-zero')
---             lsp_zero.extend_cmp()
---
---             -- And you can configure cmp even more, if you want to.
---             local cmp = require('cmp')
---             -- local cmp_action = lsp_zero.cmp_action()
---
---             local behavior = { behavior = cmp.SelectBehavior.select }
---
---             cmp.setup({
---                 formatting = lsp_zero.cmp_format(),
---                 mapping = cmp.mapping.preset.insert({
---                     ['<C-Space>'] = cmp.mapping.complete(),
---                     ['<C-e>'] = cmp.mapping.abort(),
---                     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
---                     ['<C-p>'] = cmp.mapping.select_prev_item(behavior),
---                     ['<C-n>'] = cmp.mapping.select_next_item(behavior),
---                     ['<Tab>'] = nil,
---                     ['<S-Tab>'] = nil,
---
---                     -- ['<C-u>'] = cmp.mapping.scroll_docs(-4),
---                     -- ['<C-d>'] = cmp.mapping.scroll_docs(4),
---                     -- ['<C-f>'] = cmp_action.luasnip_jump_forward(),
---                     -- ['<C-b>'] = cmp_action.luasnip_jump_backward(),
---                 }),
---                 sources = {
---                     { name = 'nvim_cmp' },
---                     { name = 'nvim_lsp' },
---                     { name = 'luasnip', option = { show_autosnippets = false } },
---                     { name = 'copilot' }
---                 }
---             })
---         end
---     },
---     -- LSP
---     {
---         'neovim/nvim-lspconfig',
---         cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
---         event = { 'BufReadPre', 'BufNewFile' },
---         dependencies = {
---             'williamboman/mason-lspconfig.nvim',
---             'hrsh7th/cmp-nvim-lsp',
---         },
---         config = function()
---             local lsp_zero = require('lsp-zero')
---             lsp_zero.extend_lspconfig()
---             local nmap = require('lusedou.keymaps').nmap
---
---             lsp_zero.on_attach(function(client, bufnr)
---                 -- see :help lsp-zero-keybindings
---                 -- to learn the available actions
---
---                 local opts = { buffer = bufnr, remap = false }
---                 nmap {
---                     'gd',
---                     command = vim.lsp.buf.definition,
---                     desc = 'view definition',
---                     opts = opts,
---                 }
---
---                 -- vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
---                 vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
---                 vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
---                 vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
---                 vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
---                 vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
---                 vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
---                 vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
---                 vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
---                 vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
---
---                 lsp_zero.default_keymaps({ buffer = bufnr })
---             end)
---
---             require('mason-lspconfig').setup({
---                 ensure_installed = {},
---                 handlers = {
---                     lsp_zero.default_setup,
---                     lua_ls = function()
---                         -- (Optional) Configure lua language server for neovim
---                         local lua_opts = lsp_zero.nvim_lua_ls()
---                         require('lspconfig').lua_ls.setup(lua_opts)
---                     end,
---                 }
---             })
---         end
---     },
--- }
-
 return {
     {
         'VonHeikemen/lsp-zero.nvim',
@@ -197,7 +68,8 @@ return {
         cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
         event = { 'BufReadPre', 'BufNewFile' },
         keys = {
-            '<leader>vca', '<leader>vd', '<leader>vws', '<leader>vrr', '<leader>vrn', '<leader>hh', '<leader>gd', 'K'
+            '<leader>vca', '<leader>vd', '<leader>vws', '<leader>vrr', '<leader>vrn', '<leader>hh', '<leader>gd', 'K',
+            '<C-h>',
         },
         dependencies = {
             { 'hrsh7th/cmp-nvim-lsp' },
@@ -205,6 +77,8 @@ return {
         },
         config = function()
             local nmap = require('lusedou.keymaps').nmap
+            local imap = require('lusedou.keymaps').imap
+
             -- This is where all the LSP shenanigans will live
             local lsp_zero = require('lsp-zero')
             lsp_zero.extend_lspconfig()
@@ -224,7 +98,10 @@ return {
 
                 nmap {
                     'gd',
-                    command = vim.lsp.buf.definition,
+                    command = function()
+                        vim.lsp.buf.definition()
+                        vim.cmd('normal! zz')
+                    end,
                     desc = 'view definition',
                     opts = opts,
                 }
@@ -263,9 +140,9 @@ return {
                     desc = 'rename',
                     opts = opts,
                 }
-
-                nmap {
-                    '<leader>hh',
+          
+                imap {
+                    '<C-h>',
                     command = vim.lsp.buf.signature_help,
                     desc = 'Go help',
                     opts = opts,
