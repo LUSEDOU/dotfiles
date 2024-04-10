7f07f0dd7205381d6e139aab12ba86555dfe766a
LUSEDOU <dolorier_uribe@hotmail.com>
2024-04-09 22:34:17 -0500

    add paths

--- zsh/.oh-my-zsh/custom/my_paths.zsh
+++ zsh/.oh-my-zsh/custom/my_paths.zsh
@@ -2,8 +2,10 @@ export PATH="/usr/local/anaconda3/bin:$PATH"
 # path+=($FDK)
 path+=($HOME/.pub-cache/bin)
 path+=($HOME/.local/bin/scripts)
-path+=('/usr/local/texlive/2024/bin/x86_64-linux')
+# export PATH="/home/lusedou/.local/share/solana/install/active_release/bin:$PATH"
+path+=($HOME/.local/share/solana/install/active_release/bin)
+# path+=('/usr/local/texlive/2024/bin/x86_64-linux')
 export PATH
 
-manpath+=('/usr/local/texlive/2024/texmf-dist/doc/man')
+# manpath+=('/usr/local/texlive/2024/texmf-dist/doc/man')
 export MANPATH
