e966163d1e10d509086e76e707620384a181c2a1
LUSEDOU <dolorier_uribe@hotmail.com>
2024-06-08 13:50:00 -0500

    feat(zsh/paths): add go and flutter

--- zsh/.oh-my-zsh/custom/my_paths.zsh
+++ zsh/.oh-my-zsh/custom/my_paths.zsh
@@ -1,7 +1,9 @@
 export PATH="/usr/local/anaconda3/bin:$PATH"
 # path+=($FDK)
+path+=($HOME/flutter/bin)
 path+=($HOME/.pub-cache/bin)
 path+=($HOME/.local/bin/scripts)
+path+=(/usr/local/go/bin)
 # export PATH="/home/lusedou/.local/share/solana/install/active_release/bin:$PATH"
 path+=($HOME/.local/share/solana/install/active_release/bin)
 # path+=('/usr/local/texlive/2024/bin/x86_64-linux')
