c082cdbbe00812360855d19ae294192b61aa90b0
dolorier_uribe@hotmail.com <ldolorier@perufarma.com.pe>
2024-06-10 17:30:20 -0500

    feat: improve build_runner

--- nvim/.config/nvim/after/ftplugin/dart.lua
+++ nvim/.config/nvim/after/ftplugin/dart.lua
@@ -8,73 +8,149 @@ cmd.autocmd {
     if vim.bo.ft ~= 'dart' then
       return
     end
-    print('Flutter: Formatting file... 1')
+
+    vim.cmd.colorscheme 'kanagawa'
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+    require 'flutter-tools.ui'.select
+
 
     local nmap = require('lusedou.keymaps').nmap
     local Job = require 'plenary.job'
     local ui = require 'fidget.progress'.handle
+
 
     nmap {
       '<leader>br',
       command = function()
         local ex = require 'flutter-tools.executable'
         ex.dart(function(path)
-          local job_ui = ui.create {
-            title = 'Dart Build Runner',
-            lsp_client = { name = 'flutter custom tools' },
+          ---@type ProgressHandle[]
+          local jobs = {}
+          local index = 1
+          jobs[1] = ui.create {
+            title = 'Building',
+            lsp_client = { name = 'dart run build_runner build' },
             progress = 0,
           }
+
+          local function printf(message)
+            vim.schedule(function()
+              print(message)
+            end)
+          end
+
+          -- local track = ''
+          -- local function bc(breadcrumb)
+          --   track = track .. ' '.. breadcrumb
+          --   printf(track)
+          -- end
+          local cwd = vim.fn.getcwd()
+          local hasError = false
+
+          local function log_fail(message)
+            local log = io.open(cwd .. '/build_runner.log', 'a')
+            if log == nil then
+              return
+            end
+            log:write(message .. '\n')
+            log:close()
+            hasError = true
+          end
+
+          local function report(data)
+            if data == '' then
+              return
+            end
+
+            local status, message = data:match("%[(%w+)%]%s(.*)")
+            local job = jobs[index]
+
+            if status ~= "INFO" then
+              if job then
+                if status == "SEVERE" then
+                  job:report {
+                    message = 'Write to build_runner.log for more details',
+                  }
+                end
+              end
+              log_fail(data)
+            else
+              if job == nil then
+                --bc('NIL')
+                return
+              end
+
+              if message:match('%.%.+$') then
+                index = index + 1
+                jobs[index] = ui.create {
+                  title = message:match("(.-)%.%.+$"),
+                  lsp_client = { name = 'dart run build_runner build' },
+                  progress = 0,
+                }
+                --bc('New')
+                return
+              end
+
+              local took = message:match("took (%d+%.?%d*%w+)")
+              if took then
+                job:report {
+                  message = 'Took ' .. took,
+                  percentage = 100,
+                }
+                jobs[index] = nil
+                index = index - 1
+                --bc('Completed')
+                return
+              end
+
+              local elapsed = message:match("(%d+%.?%d*s) elapsed")
+              local cactions, tactions = message:match("(%d+)/(%d+) actions")
+              if elapsed and cactions and tactions then
+                job:report {
+                  message = elapsed .. ' elapsed',
+                  percentage = (cactions / tactions) * 100,
+                }
+                --bc('Elapsed')
+                return
+              end
+
+              job.message = message
+              --bc('Message')
+
+              -- local title = message:match("^[%a%s]*")
+              -- job = ui.create {
+              --   title = title,
+              --   lsp_client = { name = 'dart run build_runner build' },
+              --   progress = 0,
+              -- }
+            end
+          end
           Job:new({
             ---@diagnostic disable-next-line: assign-type-mismatch
             command = path,
-            on_start = function()
-              job_ui.message = 'Start building...'
-            end,
             args = { 'run', 'build_runner', 'build', '--delete-conflicting-outputs' },
             on_stdout = function(_, data, _)
-              -- [INFO] Generating build script completed, took 347ms
-              -- [INFO] Reading cached asset graph completed, took 507ms
-              -- [INFO] Checking for updates since last build completed, took 1.7s
               if type(data) == "string" then
-                job_ui:report {
-                  title = 'Build Runner',
-                  message = data,
-                  percentage = 100,
-                }
+                report(data)
               elseif type(data) == "table" then
+                printf(table)
                 for _, v in ipairs(data) do
-                  job_ui:report {
-                    title = 'Build Runner',
-                    message = v,
-                    percentage = 100,
-                  }
+                  report(v)
                 end
               end
             end,
             on_stderr = function(_, data, _)
-              if type(data) == "string" then
-                job_ui:report {
-                  title = 'Build Runner',
-                  message = data,
-                  percentage = 100,
-                }
-              elseif type(data) == "table" then
-                for _, v in ipairs(data) do
-                  job_ui:report {
-                    title = 'Build Runner',
-                    message = v,
-                    percentage = 100,
-                  }
-                end
-              end
+              -- write to log
+              log_fail(data)
             end,
             on_exit = vim.schedule_wrap(function()
-              -- job_ui:report {
-              --     title = 'Build Runner',
-              --     message = 'Build completed',
-              --     percentage = 100,
-              -- }
-              job_ui:finish()
+              jobs = {}
+              if hasError then
+                -- split window vertically and open cwd .. '/build_runner.log'
+                vim.cmd('vsplit ' .. cwd .. '/build_runner.log')
+              end
             end)
           }):start()
         end)
