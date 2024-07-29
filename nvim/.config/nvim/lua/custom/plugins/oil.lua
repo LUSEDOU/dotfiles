6ba26355cc1995194f0f468ba49e169b9e8f1eab
LUSEDOU <dolorier_uribe@hotmail.com>
2024-07-29 18:54:26 -0500

    oil

---
+++ nvim/.config/nvim/lua/custom/plugins/oil.lua
@@ -0,0 +1,9 @@
+return {
+  'stevearc/oil.nvim',
+  opts = {
+    default_file_explorer = false,
+  },
+  -- Optional dependencies
+  -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+}
