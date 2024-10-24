664918f3b25fc6ed3bc138a6f5c08817c59fb1bf
dolorier_uribe@hotmail.com <ldolorier@perufarma.com.pe>
2024-10-24 16:27:01 -0500

    feat(nvim): dadbod

--- nvim/.config/nvim/lua/custom/plugins/dadbod.lua
+++ nvim/.config/nvim/lua/custom/plugins/dadbod.lua
@@ -1,19 +1,19 @@
 return {
   'kristijanhusak/vim-dadbod-ui',
-  enabled = false,
+  -- enabled = false,
   dependencies = {
     {
       "tpope/vim-dadbod",
       lazy = true,
-      enabled = false,
+      cmd = { 'DB', 'DBUI' },
     },
     {
       "kristijanhusak/vim-dadbod-completion",
       ft = { 'sql', 'mysql', 'plsql' },
       lazy = true,
-      enabled = false,
     },
   },
+  cmd = { 'DBUIToggle', 'DBUI', 'DBUIFindBuffer', 'DBUIRenameBuffer' },
   init = function()
     vim.g.db_ui_use_nerd_fonts = 1
   end
