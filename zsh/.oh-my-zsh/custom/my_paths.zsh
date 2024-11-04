30289de6bb931ef001c30be977fe070b82a619f2
LUSEDOU <dolorier_uribe@hotmail.com>
2024-11-04 11:31:08 -0500

    feat(zsh): add go path

--- zsh/.oh-my-zsh/custom/my_paths.zsh
+++ zsh/.oh-my-zsh/custom/my_paths.zsh
@@ -2,5 +2,6 @@ export PATH="/usr/local/anaconda3/bin:$PATH"
 # path+=($FDK)
 path+=($HOME/.pub-cache/bin)
 path+=($HOME/.local/bin/scripts)
+path+=($(go env GOPATH)/bin)
 # path+=('/Users/lusedou/fvm/default/bin')
 export PATH
